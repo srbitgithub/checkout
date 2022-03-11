90b444868f43ada751dfe16f8799351bfa0d49a5
srbitgithub <sr_bit@hotmail.com>
2022-03-11 22:40:51 +0100

    Group C and D solved

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
 function Checkout() {
-    this.main = function(myString){
-        if(myString.length == 0) return 0;
-        if(myString.length == 1) return this.lookChar(myString)
-        if(myString.length == 2) return this.byChars(myString)
-        if(myString.length == 3) return this.byChars(myString)
-        if(myString.length == 4) return this.byChars(myString)
-    }    
-    
-    this.lookChar = function(char){
-        if (char == 'A') return 50
-        if (char == 'B') return 30
-        if (char == 'C') return 20
-        if (char == 'D') return 15
-    }
-
-    this.byChars = function(myString){
-        let myArray = myString.split('')
-        const arrayValues = myArray.map(this.lookChar)
-        
-        let total = 0
-        arrayValues.forEach (function(n){
-            total += n
-        });
-        
-        return total
-    }
 
     this.findItem = function(myArray, itemToFind){
         const newArray = myArray.filter(item => item == itemToFind)
@@ -39,13 +13,12 @@ function Checkout() {
         const itemsB = this.findItem(myArray, 'B')
         const itemsC = this.findItem(myArray, 'C')
         const itemsD = this.findItem(myArray, 'D')
-        console.log('itemsB: ' + itemsB)
 
-        if (itemsA.length > 0) total = total + this.calculateTotal(itemsA, 'A')
-        if (itemsB.length > 0) total = total + this.calculateTotal(itemsB, 'B')
-        console.log('desde separeByCharGroups: ' + total)
-        /*total = total + this.calculateTotal(itemsC, 'C')
-        total = total + this.calculateTotal(itemsD, 'D')*/
+        total = total + this.calculateTotal(itemsA, 'A')
+        total = total + this.calculateTotal(itemsB, 'B')
+        total = total + this.calculateTotal(itemsC, 'C')
+        total = total + this.calculateTotal(itemsD, 'D')
+
         return total
     }
 
@@ -54,8 +27,8 @@ function Checkout() {
         let totalReminderGroupA = 0
         let totalGroupB = 0
         let totalReminderGroupB = 0
-        //let totalGroupC = 0
-        //let totalGroupD = 0
+        let totalGroupC = 0
+        let totalGroupD = 0
 
         if (charGroup == 'A')
         {
@@ -64,23 +37,22 @@ function Checkout() {
         }
         if (charGroup == 'B')
         {
-            console.log('longitud B: ' + myArray.length)
             if (myArray.length > 1) totalGroupB = this.calculateGroupB(myArray)
-            //if (myArray.length = 1) totalGroupB = 30
             totalReminderGroupB = this.calculateReminderGroupB(myArray)
-            console.log('totalReminderGroupB: ' + totalReminderGroupB)
         }
-        /*if (charGroup == 'C')
+        if (charGroup == 'C')
         {
             if (myArray.length > 0) totalGroupC = this.calculateGroupC(myArray)
         }
         if (charGroup == 'D')
         {
             if (myArray.length > 0) totalGroupD = this.calculateGroupD(myArray)
-        }*/
+        }
 
-        return  (totalGroupA + totalReminderGroupA + totalGroupB + totalReminderGroupB)
+        return  (totalGroupA + totalReminderGroupA + totalGroupB + totalReminderGroupB +
+                totalGroupC + totalGroupD)
     }
+
 
     this.calculateGroupA = function(myArray){
         const result = Math.floor(myArray.length / 3)
@@ -109,9 +81,6 @@ function Checkout() {
     this.calculateGroupD = function(myArray){
         return myArray.length * 15
     }
-
-
-
 }
 
 module.exports = Checkout
--- main.spec.js
+++ main.spec.js
@@ -5,42 +5,43 @@ describe('checkout', ()=>{
     const checkout = new Checkout()
 
     it ('Return 0 if the string is empty', ()=>{
-        const result = checkout.main('')
+        const result = checkout.separeByCharGroups('')
         expect(result).toBe(0)
     });
 
     it ('Return 50 if the string is A', ()=>{
-        const result = checkout.main('A')
+        const result = checkout.separeByCharGroups('A')
         expect(result).toBe(50)
     });
 
     it ('Return 30 if the string is B', ()=>{
-        const result = checkout.main('B')
+        const result = checkout.separeByCharGroups('B')
         expect(result).toBe(30)
     });
 
     it ('Return 20 if the string is C', ()=>{
-        const result = checkout.main('C')
+        const result = checkout.separeByCharGroups('C')
+
         expect(result).toBe(20)
     });
 
     it ('Return 15 if the string is D', ()=>{
-        const result = checkout.main('D')
+        const result = checkout.separeByCharGroups('D')
         expect(result).toBe(15)
     });
 
     it ('Return 80 if the string is AB', ()=>{
-        const result = checkout.main('AB')
+        const result = checkout.separeByCharGroups('AB')
         expect(result).toBe(80)
     });
 
     it ('Return 65 if the string is CDB', ()=>{
-        const result = checkout.main('CDB')
+        const result = checkout.separeByCharGroups('CDB')
         expect(result).toBe(65)
     });
 
     it ('Return 115 if the string is CDBA', ()=>{
-        const result = checkout.main('CDBA')
+        const result = checkout.separeByCharGroups('CDBA')
         expect(result).toBe(115)
     });
 
@@ -75,5 +76,13 @@ describe('checkout', ()=>{
         expect(result).toBe(175)
     });
 
+    it ('Return 190 if the string is AAABBD', ()=>{
+        const result = checkout.separeByCharGroups('AAABBD')
+        expect(result).toBe(190)
+    });
 
+    it ('Return 190 if the string is DABABA', ()=>{
+        const result = checkout.separeByCharGroups('DABABA')
+        expect(result).toBe(190)
+    });
 });
