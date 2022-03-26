de49c3ae635534521c93dcf1cb45c8242e6211e1
srbitgithub <sr_bit@hotmail.com>
2022-03-26 13:33:19 +0100

    Deep refactor

--- main.spec.js
+++ main.spec.js
@@ -6,104 +6,87 @@ describe('checkout', () =>{
 
     it ('Return 0 if the string is empty', ()=>{
         const checkout = new Checkout('')
-        const result = checkout.main()
-        expect(result).toBe(0)
+        expect(checkout.total).toBe(0)
     });
 
     it ('Return 50 if the string is A', ()=>{
         const checkout = new Checkout('A')
-        const result = checkout.main()
-        expect(result).toBe(50)
+        expect(checkout.total).toBe(50)
     });
 
     it ('Return 30 if the string is B', ()=>{
         const checkout = new Checkout('B')
-        const result = checkout.main()
-        expect(result).toBe(30)
+        expect(checkout.total).toBe(30)
     });
 
     it ('Return 20 if the string is C', ()=>{
         const checkout = new Checkout('C')
-        const result = checkout.main()
-        expect(result).toBe(20)
+        expect(checkout.total).toBe(20)
     });
 
     it ('Return 15 if the string is D', ()=>{
         const checkout = new Checkout('D')
-        const result = checkout.main()
-        expect(result).toBe(15)
+        expect(checkout.total).toBe(15)
     });
 
     it ('Return 80 if the string is AB', ()=>{
         const checkout = new Checkout('AB')
-        const result = checkout.main()
-        expect(result).toBe(80)
+        expect(checkout.total).toBe(80)
     });
 
     it ('Return 65 if the string is CDB', ()=>{
         const checkout = new Checkout('CDB')
-        const result = checkout.main()
-        expect(result).toBe(65)
+        expect(checkout.total).toBe(65)
     });
 
     it ('Return 115 if the string is CDBA', ()=>{
         const checkout = new Checkout('CDBA')
-        const result = checkout.main()
-        expect(result).toBe(115)
+        expect(checkout.total).toBe(115)
     });
 
     it ('Return 100 if the string is AA', ()=>{
         const checkout = new Checkout('AA')
-        const result = checkout.main()
-        expect(result).toBe(100)
+        expect(checkout.total).toBe(100)
     });
 
     it ('Return 130 if the string is AAA', ()=>{
         const checkout = new Checkout('AAA')
-        const result = checkout.main()
-        expect(result).toBe(130)
+        expect(checkout.total).toBe(130)
     });
 
     it ('Return 180 if the string is AAAA', ()=>{
         const checkout = new Checkout('AAAA')
-        const result = checkout.main()
-        expect(result).toBe(180)
+        expect(checkout.total).toBe(180)
     });
 
     it ('Return 230 if the string is AAAAA', ()=>{
         const checkout = new Checkout('AAAAA')
-        const result = checkout.main()
-        expect(result).toBe(230)
+        expect(checkout.total).toBe(230)
     });
 
     it ('Return 260 if the string is AAAAAA', ()=>{
         const checkout = new Checkout('AAAAAA')
-        const result = checkout.main()
-        expect(result).toBe(260)
+        expect(checkout.total).toBe(260)
     });
 
     it ('Return 160 if the string is AAAB', ()=>{
         const checkout = new Checkout('AAAB')
-        const result = checkout.main()
-        expect(result).toBe(160)
+        expect(checkout.total).toBe(160)
     });
 
     it ('Return 175 if the string is AAABB', ()=>{
         const checkout = new Checkout('AAABB')
-        const result = checkout.main()
-        expect(result).toBe(175)
+        expect(checkout.total).toBe(175)
     });
 
     it ('Return 190 if the string is AAABBD', ()=>{
         const checkout = new Checkout('AAABBD')
-        const result = checkout.main()
-        expect(result).toBe(190)
+        expect(checkout.total).toBe(190)
     });
 
     it ('Return 190 if the string is DABABA', ()=>{
         const checkout = new Checkout('DABABA')
-        const result = checkout.main()
-        expect(result).toBe(190)
+        expect(checkout.total).toBe(190)
     });
 });
 
--- mainClasses.js
+++ mainClasses.js
@@ -1,31 +1,32 @@
 class Checkout {
-    constructor(myString){
+    constructor(myString, isIncremental){
         this._myString = myString
-
         this._incrementalString = ''
+
+        this.VALUE_A = 50
+        this.VALUE_A_BY_3 = 130
+        this.VALUE_B = 30
+        this.VALUE_B_BY_2 = 45
+        this.VALUE_C = 20
+        this.VALUE_D = 15
+
+        this.total =  this.main()
+        return this.total
     }
 
-    get myString(){
-        return this._myString
-    }
-
-    set myString(newMyString){
-        this._myString = newMyString;
-    }
-
-    main(){
+    main = () => {
         let myArray = this._myString.split('')
         return this.calculateTotal(myArray)
     }
 
-    mainIncremental(newString){
+    mainIncremental = (newString) => {
         const currentIncrementalString = this._incrementalString;
         this._incrementalString = currentIncrementalString + newString
         const myArray = this._incrementalString.split('')
         return this.calculateTotal(myArray)
     }
 
-    calculateTotal(myArray){
+    calculateTotal = (myArray) => {
         let total = 0
         const itemsA = this.findItem(myArray, 'A')
         const itemsB = this.findItem(myArray, 'B')
@@ -36,60 +37,38 @@ class Checkout {
         total = total + this.calculateItemsB(itemsB)
         total = total + this.calculateItemsC(itemsC)
         total = total + this.calculateItemsD(itemsD)
-
         return total
     }
 
-    findItem(myArray, itemToFind){
-        return myArray.filter(item => item == itemToFind)
-    }
+    findItem = (myArray, itemToFind) => myArray.filter(item => item == itemToFind)
 
-    calculateItemsA(myArray){
+    calculateItemsA = (myArray) =>{
         const totalGroupA = this.calculateGroupA(myArray)
         const totalReminderGroupA = this.calculateReminderGroupA(myArray)
-
         return totalGroupA + totalReminderGroupA
     }
 
-    calculateItemsB(myArray){
+    calculateItemsB = (myArray) => {
         const totalGroupB = this.calculateGroupB(myArray)
         const totalReminderGroupB = this.calculateReminderGroupB(myArray)
-
         return totalGroupB + totalReminderGroupB
     }
 
-    calculateItemsC(myArray){
-        return this.calculateGroupC(myArray)
-    }
+    calculateItemsC = (myArray) => this.calculateGroupC(myArray)
 
-    calculateItemsD(myArray){
-        return this.calculateGroupD(myArray)
-    }
+    calculateItemsD = (myArray) => this.calculateGroupD(myArray)
 
-    
-    calculateGroupA(myArray){
-        return Math.floor(myArray.length / 3) * 130
-    }
+    calculateGroupA = (myArray) => Math.floor(myArray.length / 3) * this.VALUE_A_BY_3
 
-    calculateReminderGroupA(myArray){
-        return Math.floor(myArray.length % 3) * 50
-    }
+    calculateReminderGroupA = (myArray) => Math.floor(myArray.length % 3) * this.VALUE_A
 
-    calculateGroupB(myArray){
-        return Math.floor(myArray.length / 2) * 45
-    }
+    calculateGroupB = (myArray) => Math.floor(myArray.length / 2) * this.VALUE_B_BY_2
 
-    calculateReminderGroupB(myArray){
-        return Math.floor(myArray.length % 2) * 30
-    }
+    calculateReminderGroupB = (myArray) => Math.floor(myArray.length % 2) * this.VALUE_B
 
-    calculateGroupC(myArray){
-        return myArray.length * 20
-    }
+    calculateGroupC = (myArray) => myArray.length * this.VALUE_C
 
-    calculateGroupD(myArray){
-        return myArray.length * 15
-    }
+    calculateGroupD = (myArray) => myArray.length * this.VALUE_D
 }
 
 module.exports = Checkout
